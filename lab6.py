2bf2d733d3c77905dca4e8f124fd705f284a2791
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-03-07 22:22:07 -0700

    reversion for proper lab pull and push

--- lab6.py
+++ lab6.py
@@ -1,6 +1,5 @@
-# Name: Josh Gallardo
-# lab6.py
-
+#Name: Josh Gallardo
+#lab6.py
 
 """
 The password encoder should take in an 8-digit password in string format containing only integers. 
@@ -8,29 +7,27 @@ After passing the password into the encoder, the encoder stores the encoded pass
 variable, with each digit being shifted up by 3 numbers.
 """
 
-
 def encoder(user_pwd):
-    my_dict = {"0": 3, "1": 4, "2": 5, "3": 6, "4": 7, "5": 8, "6": 9, "7": 0, "8": 1, "9": 2}
+
+    my_dict = {"0":3, "1":4, "2":5, "3":6, "4":7, "5":8, "6":9, "7":0, "8":1, "9":2 }
 
     result = ""
     for index in user_pwd:
         my_array = my_dict[index]
-        result = result + str(my_array)
-    return result
+        result = result + str(my_array) 
+    return(result)
+
+def decoder(encoder):
+    my_dict = {"0":3, "1":4, "2":5, "3":6, "4":7, "5":8, "6":9, "7":0, "8":1, "9":2 }
 
 
-# Geoffrey Clark's decoder
-def decoder(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        unshifted_digit = str((int(digit)-3) % 10)
-        decoded_password += unshifted_digit
-    return decoded_password
+
 
 
 def menu():
+
     integer_list = []
-    while (True):
+    while(True):
         print("Menu")
         print("-------------")
         print("1. Encode")
@@ -43,23 +40,24 @@ def menu():
         if user_choice == 1:
             print("Please enter your password to encode: ")
             user_pwd = input()
-            encodervalue = encoder(user_pwd)
-            print("Your password has been encoded and stored!\n")
+            encoderValue = encoder(user_pwd)
 
+            print("Your password has been encoded and stored!")
         elif user_choice == 2:
-            decodedvalue = decoder(encodervalue)
-            print(f"The encoded password is {encodervalue}, and the original password is {decodedvalue}.")
+            pass
 
         elif user_choice == 3:
             break
 
-        # print("The encoded password is", result,"and the original password is", user_pwd,".", end="")
+
+
+
+        #print("The encoded password is", result,"and the original password is", user_pwd,".", end="")
         else:
             print("Please enter a valid input. ")
             pass
 
-
 if __name__ == "__main__":
-    menu()
+     menu()
 
 
